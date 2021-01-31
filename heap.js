7d237fae879b08d67c2cf8dec43e4be7e1c9dc7b
Dhruv <dhruvrawat2611@gmail.com>
2021-01-31 16:31:10 +0530

    Fixed bug

--- heap.js
+++ heap.js
@@ -2,61 +2,71 @@
 export { BinaryHeap }
 
 class BinaryHeap {
+
     constructor() {
         this.heap = [];
     }
-    size() {
-        return this.heap.length;
-    }
-    empty() {
-        if (this.size() == 0) {
-            return true;
-        }
-        return false;
-    }
+
     insert(value) {
         this.heap.push(value);
         this.bubbleUp();
     }
+
+    size() {
+        return this.heap.length;
+    }
+
+    empty(){
+        return ( this.size()===0 );
+    }
+
+    //using iterative approach
     bubbleUp() {
         let index = this.size() - 1;
+
         while (index > 0) {
-            let element = this.heap[index];
-            let parentIndex = Math.floor((index - 1) / 2);
-            let parent = this.heap[parentIndex];
-            if (parent[0] > element[0]) {
-                break;
-            }
+            let element = this.heap[index],
+                parentIndex = Math.floor((index - 1) / 2),
+                parent = this.heap[parentIndex];
+
+            if (parent[0] >= element[0]) break;
             this.heap[index] = parent;
             this.heap[parentIndex] = element;
+            index = parentIndex
         }
     }
+
     extractMax() {
-        const mx = this.heap[0];
+        const max = this.heap[0];
         const tmp = this.heap.pop();
-        if (!this.empty()) {
+        if(!this.empty()) {
             this.heap[0] = tmp;
             this.sinkDown(0);
         }
-        return mx;
+        return max;
     }
-    sinkDown(index) {
-        let left = 2 * index + 1;
-        let right = 2 * index + 2;
-        let largest = index;
-        let length = this.size() - 1;
-        if (left <length&& this.heap[left][0]>this.heap[largest][0]){
-            largest=left;
-        }
-        if(right<length&&this.heap[right][0]>this.heap[largest][0]){
-            largest=right;
-        }
-        if(largest!==index){
-            let tmp=this.heap[largest];
-            this.heap[largest]=this.heap[index];
-            this.heap[index]=tmp;
-            this.sinkDown(largest);
-        }
 
+    sinkDown(index) {
+
+        let left = 2 * index + 1,
+            right = 2 * index + 2,
+            largest = index;
+        const length = this.size();
+
+        // console.log(this.heap[left], left, length, this.heap[right], right, length, this.heap[largest]);
+
+        if (left < length && this.heap[left][0] > this.heap[largest][0]) {
+            largest = left
+        }
+        if (right < length && this.heap[right][0] > this.heap[largest][0]) {
+            largest = right
+        }
+        // swap
+        if (largest !== index) {
+            let tmp = this.heap[largest];
+            this.heap[largest] = this.heap[index];
+            this.heap[index] = tmp;
+            this.sinkDown(largest)
+        }
     }
 }

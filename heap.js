0b6c946b843dd76e03556717f44fc87b32e0ef91
Dhruv <dhruvrawat2611@gmail.com>
2021-01-31 14:45:09 +0530

    Added extract max and heapify function

--- heap.js
+++ heap.js
@@ -15,5 +15,45 @@ class BinaryHeap {
         this.heap.push(value);
         this.bubbleUp();
     }
-   
+    bubbleUp() {
+        let index = this.size() - 1;
+        while (index > 0) {
+            let element = this.heap[index];
+            parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.heap[parentIndex];
+            if (parent[0] > element[0]) {
+                break;
+            }
+            this.heap[index] = parent;
+            this.heap[parentIndex] = element;
+        }
+    }
+    extractMax() {
+        const mx = this.heap[0];
+        const tmp = this.heap.pop();
+        if (!this.empty()) {
+            this.heap[0] = temp;
+            this.sinkDown(0);
+        }
+        return mx;
+    }
+    sinkDown(index) {
+        let left = 2 * index + 1;
+        let right = 2 * index + 2;
+        let largest = index;
+        let length = this.size() - 1;
+        if (left <length&& this.heap[left][0]>this.heap[largest][0]){
+            largest=left;
+        }
+        if(right<length&&this.heap[right][0]>this.heap[largest][0]){
+            largest=right;
+        }
+        if(largest!==index){
+            let tmp=this.heap[largest];
+            this.heap[largest]=this.heap[index];
+            this.heap[index]=temp;
+            this.sinkDown(largest);
+        }
+
+    }
 }
